dcd726013fe3809f3e69c3e52fe16d117b7a6c09
Peter Streef <p.streef@gmail.com>
2024-02-25 09:41:37 +0100

    Add controller

---
+++ src/main/java/io/moderne/scouting/cookies/user/UserController.java
@@ -0,0 +1,32 @@
+package io.moderne.scouting.cookies.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User findUser(@PathVariable String id) {
+        return userService.findUser(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+    }
+}
--- src/main/java/io/moderne/scouting/cookies/user/UserService.java
+++ src/main/java/io/moderne/scouting/cookies/user/UserService.java
@@ -9,11 +9,29 @@ import java.util.Map;
 public class UserService {
 
     private Map<String, User> db = new HashMap<>();
+
     public User createUser(User user) {
         db.put(user.id(), user);
         return user;
     }
+
     public User findUser(String id) {
         return db.get(id);
+    }
+
+    public User updateUser(String id, User user) {
+        if (user.id() != null && !user.id().equals(id)) {
+            throw new IllegalArgumentException("User id cannot be changed");
+        }
+        User old = db.get(id);
+        if (old == null) {
+            return null;
+        }
+        db.put(user.id(), user);
+        return user;
+    }
+
+    public void deleteUser(String id) {
+        db.remove(id);
     }
 }
